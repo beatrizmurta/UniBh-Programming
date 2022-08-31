786440fe4188d895ff4ccacc21dc8f9f16ff7654
beatrizmurta <99887461+beatrizmurta@users.noreply.github.com>
2022-08-31 09:45:39 -0300

    Diferença part2
    Este programa calcula o seu salário líquido a partir do seu salário bruto e faz a diferença entre o salário líquido e o total do valor dos boletos. Nele há entrada, processamento e saída.

--- Aula2F_2v3/src/aula2f_2v3/Aula2F_2v3.java
+++ Aula2F_2v3/src/aula2f_2v3/Aula2F_2v3.java
@@ -2,27 +2,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package aula2f_2v2;
+package aula2f_2v3;
 import java.util.Scanner;
 import java.io.*;
 /**
  *
  * @author beatr
  */
-public class Aula2F_2v2 {
+public class Aula2F_2v3 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        double boleto1, boleto2, boleto3, boleto4, totalboleto, salarioliquido, diferenca;
+        double boleto1, boleto2, boleto3, boleto4, totalboleto, salariobruto, salarioliquido, diferenca;
         Scanner ler = new Scanner (System.in);
         
-        System.out.println("Digite o seu salário líquido: ");
-        salarioliquido = ler.nextDouble();
+        System.out.println("Digite o se salário bruto: ");
+        salariobruto = ler.nextDouble();
         
-        System.out.println("Digite o valor 1° boleto: ");
+        System.out.println("Digite o valor do 1° boleto: ");
         boleto1 = ler.nextDouble();
         
         System.out.println("Digite o valor do 2° boleto: ");
@@ -34,11 +34,14 @@ public class Aula2F_2v2 {
         System.out.println("Digite o valor do 4° boleto: ");
         boleto4 = ler.nextDouble();
         
+        salarioliquido = salariobruto*0.86;
+        System.out.println("O seu salário líquido é: " +salarioliquido);
+        
         totalboleto = boleto1+boleto2+boleto3+boleto4;
-        System.out.println("O total da soma dos boletos é: " +totalboleto);
+        System.out.println("O tota da soma dos boletos é: " +totalboleto);
         
         diferenca = salarioliquido-totalboleto;
-        System.out.println("A diferença é de: " +diferenca);
+        System.out.println("A diferença do seu salário líquido e o total dos boletos é: "+diferenca);
     }
     
 }
