47dbfb08c6e4374037568ce188d2c13bd0cf1482
beatrizmurta <99887461+beatrizmurta@users.noreply.github.com>
2022-08-31 09:29:10 -0300

    Diferença do salário líquido e dos boletos
    Este programa calcula a diferença do salário líquido com o valor total somado dos boletos. Nele há entrada processamento e saída.

---
+++ Aula2F_2v2/src/aula2f_2v2/Aula2F_2v2.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula2f_2v2;
+import java.util.Scanner;
+import java.io.*;
+/**
+ *
+ * @author beatr
+ */
+public class Aula2F_2v2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double boleto1, boleto2, boleto3, boleto4, totalboleto, salarioliquido, diferenca;
+        Scanner ler = new Scanner (System.in);
+        
+        System.out.println("Digite o seu salário liquido: ");
+        salarioliquido = ler.nextDouble();
+        
+        System.out.println("Digite o valor 1° boleto: ");
+        boleto1 = ler.nextDouble();
+        
+        System.out.println("Digite o valor do 2° boleto: ");
+        boleto2 = ler.nextDouble();
+        
+        System.out.println("Digite o valor do 3° boleto: ");
+        boleto3 = ler.nextDouble();
+        
+        System.out.println("Digite o valor do 4° boleto: ");
+        boleto4 = ler.nextDouble();
+        
+        totalboleto = boleto1+boleto2+boleto3+boleto4;
+        System.out.println("O total da soma dos boletos é: " +totalboleto);
+        
+        diferenca = salarioliquido-totalboleto;
+        System.out.println("A diferença é de: " +diferenca);
+    }
+    
+}
